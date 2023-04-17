a9072f6abf0d618a95ddf6d82c38e81a17646e0f
Marco <marcoantonionemetala@gmail.com>
2023-04-17 11:22:57 -0300

    Dois estados criados
        Implementação do estado de Menu e do estado de Play, super simples para começar a montar a logica de uma maquina finita de estados;

---
+++ include/PlayState.h
@@ -0,0 +1,18 @@
+#include "GameState.h"
+
+class PlayState : public GameState
+{
+private:
+    static const std::string s_playID;
+public:
+    virtual void update();
+    virtual void render();
+
+    virtual bool onEnter();
+    virtual bool onExit();
+
+    virtual std::string getStateID() const {return s_playID;}
+
+};
+
+
