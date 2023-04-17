7b0e9a661e2e9ee2ec80fc22dec7fd7f0577fb9c
Marco <marcoantonionemetala@gmail.com>
2023-04-17 11:04:51 -0300

    Classe base para estados de jogo

--- src/Game.cpp
+++ src/Game.cpp
@@ -15,6 +15,7 @@ da forma correta (de acordo com as boas práticas de programação);
 Game* Game::s_pInstance = 0;
 //InputHandler* InputHandler::s_pInstance = 0;
 
+
 bool Game::init(const char* title,int xpos, int ypos, int width, int height, int flags) 
 {
     if (SDL_Init(SDL_INIT_EVERYTHING) == 0)
@@ -42,6 +43,7 @@ bool Game::init(const char* title,int xpos, int ypos, int width, int height, int
                 m_gameObjects.push_back(new Player(new LoaderParams(300,300,128,82,"animate")));
                 m_gameObjects.push_back(new Player(new LoaderParams(100,100,128,82, "animate")));
                 
+
                 // A proxima linha foi apenas um teste com outro sprite-sheet;
                 //TheTextureManager::Instance()->load("teste_animacao.png","teste",m_pRenderer); 
             }
